67300f23e5fbd751918937a0ab4cf490a17e109a
Edward Wong <edward.wong@live.ca>
2024-06-05 13:52:03 -0400

    Creating first React component

---
+++ my-app/src/components/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export default function Landing(){
+    return(
+        <div>
+            <h1> This is a Landing Page </h1>
+            <p> More tests </p>
+        </div>
+    );
+}
