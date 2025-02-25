185601fc2711361ad3bb6b19a348fc7013859037
Edward Wong <edward.wong@live.ca>
2025-02-25 03:04:07 -0500

    Updating project with React + Vite + Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import { NavBar } from "./components/NavBar";
+import { MobileMenu } from "./components/MobileMenu";
+import { About } from "./components/sections/About";
+import { Home } from "./components/sections/Home";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <>
+    {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+    <div className={`min-h-screen transition-opacity duration-700 ${
+      isLoaded ? "opacity-100" : "opacity-0"} 
+      bg-black text-gray-100`}
+    >
+      <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home />
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,22 @@
+export const Home = () => {
+    return (
+    <section id="home" className="min-h-screen flex items-center justify-center relative">
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right"> Hi, I'm Edward </h1>
+
+            <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                I'm a full-stack developer based in Toronto, passionate about building high-performance solutions. I focus on creating seamless user experiences with efficient and scalable technology.
+            </p>
+            <div className="flex justify-center space-x-4">
+                <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
+                    View Projects
+                </a>
+
+                <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10">
+                    Contact Me
+                </a>
+            </div>
+        </div>
+    </section>
+    );
+}
