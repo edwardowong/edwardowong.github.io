d1e8e3b99029dda43795587c9d3ba2e584aecfa9
Edward Wong <edward.wong@live.ca>
2025-02-26 12:49:10 -0500

    Added scrolling animation and removed portrait background

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,5 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const About = () => {
     
     const frontendSkills = ["JavaScript", "TypeScript", "HTML", "CSS", "React", "Figma"]
@@ -5,68 +7,71 @@ export const About = () => {
     
     return (
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
-        <div className="max-w-3xl mx-auto px-4">
-            <h2 className="text-3xl font-bond mb-8 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent text-center">
-                About Me
-            </h2>
-            <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-                <p className="text-gray-300 mb-6">
-                    Experienced developer with a passion for building scalable web applications and creating innovative solutions. With experience in developing software, optimizing processes, and enhancing user experiences, I thrive on solving complex challenges and improving system efficiency. I enjoy working with clients, actively listening to their needs, and delivering effective solutions that drive efficiency and impact.
-                </p>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4"> Frontend </h3>
-                        <div className="flex flex-wrap gap-2">
-                            {frontendSkills.map((tech, key) => (
-                                <span key={key} 
-                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                                    {tech}
-                                </span>
-                            ))}
-                        </div>
-                    </div>
-                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                    <h3 className="text-xl font-bold mb-4"> Backend </h3>
-                        <div className="flex flex-wrap gap-2">
-                        {backendSkills.map((tech, key) => (
-                            <span key={key} 
-                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                                {tech}
-                            </span>
-                            ))}
-                        </div>
-                    </div>
-                </div>
+            <RevealOnScroll>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4"> Education </h3>
-                        <ul className="list-disc list-inside text-gray-300 space-y-2">
-                            <li>
-                                <strong> B.Sc
-                                    Major in Computer Science, Minor in Game Development - Western University (2018-2023)
-                                </strong>
-                                <p>
-                                Relevant Courses: Data Structures, Algorithms, Software Engineering, Systems Programming, Operating Systems, Databases, AI, Networks, Graphics, HCI, Game Programming, Game Design    
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4"> Work Experience </h3>
-                        <div className="space-y-4 text-gray-300">
-                            <div>
-                                <li>
-                                    <strong> Developer - CGI Inc. (2021-2022) </strong>
-                                    <p> Led software deployment, managed documentation and provided on-call support for a Dynamics CRM project. </p> 
-                                    <p> Developed and optimized test automation, features for a query tool, and a payment processing system, significantly improving efficiency and user experience.</p>
-                                </li>
+                <div className="max-w-3xl mx-auto px-4">
+                    <h2 className="text-3xl font-bond mb-8 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent text-center">
+                        About Me
+                    </h2>
+                    <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                        <p className="text-gray-300 mb-6">
+                            Experienced developer with a passion for building scalable web applications and creating innovative solutions. With experience in developing software, optimizing processes, and enhancing user experiences, I thrive on solving complex challenges and improving system efficiency. I enjoy working with clients, actively listening to their needs, and delivering effective solutions that drive efficiency and impact.
+                        </p>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className="text-xl font-bold mb-4"> Frontend </h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {frontendSkills.map((tech, key) => (
+                                        <span key={key} 
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4"> Backend </h3>
+                                <div className="flex flex-wrap gap-2">
+                                {backendSkills.map((tech, key) => (
+                                    <span key={key} 
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                        {tech}
+                                    </span>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+                            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                                <h3 className="text-xl font-bold mb-4"> Education </h3>
+                                <ul className="list-disc list-inside text-gray-300 space-y-2">
+                                    <li>
+                                        <strong> B.Sc
+                                            Major in Computer Science, Minor in Game Development - Western University (2018-2023)
+                                        </strong>
+                                        <p>
+                                        Relevant Courses: Data Structures, Algorithms, Software Engineering, Systems Programming, Operating Systems, Databases, AI, Networks, Graphics, HCI, Game Programming, Game Design    
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                                <h3 className="text-xl font-bold mb-4"> Work Experience </h3>
+                                <div className="space-y-4 text-gray-300">
+                                    <div>
+                                        <li>
+                                            <strong> Developer - CGI Inc. (2021-2022) </strong>
+                                            <p> Led software deployment, managed documentation and provided on-call support for a Dynamics CRM project. </p> 
+                                            <p> Developed and optimized test automation, features for a query tool, and a payment processing system, significantly improving efficiency and user experience.</p>
+                                        </li>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
+            </RevealOnScroll>
+        </section>
     );
 };
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,26 +1,29 @@
 import Portrait from '../../assets/portrait.svg?react';
+import { RevealOnScroll } from '../RevealOnScroll';
 
 export const Home = () => {
     return (
     <section id="home" className="min-h-screen flex items-center justify-center relative">
-        <div className="text-center z-10 px-4">
-            <h1 className="text-5xl md:text-7xl font-bold mt-12 mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right"> Hi, I'm Edward </h1>
-            
-            <Portrait className="mb-6" />
+        <RevealOnScroll>
+            <div className="text-center z-10 px-4">
+                <h1 className="text-5xl md:text-7xl font-bold mt-12 mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right"> Hi, I'm Edward </h1>
+                
+                <Portrait className="mb-6" />
 
-            <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-                I'm a software developer based in Toronto who enjoys creating efficient and impactful solutions. With a strong focus on problem-solving and collaboration, I strive to build systems that enhance user experiences and streamline processes.
-            </p>
-            <div className="flex justify-center space-x-4">
-                <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
-                    View Projects
-                </a>
+                <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                    I'm a software developer based in Toronto who enjoys creating efficient and impactful solutions. With a strong focus on problem-solving and collaboration, I strive to build systems that enhance user experiences and streamline processes.
+                </p>
+                <div className="flex justify-center space-x-4">
+                    <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
+                        View Projects
+                    </a>
 
-                <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10">
-                    Contact Me
-                </a>
+                    <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10">
+                        Contact Me
+                    </a>
+                </div>
             </div>
-        </div>
+        </RevealOnScroll>
     </section>
     );
 }
